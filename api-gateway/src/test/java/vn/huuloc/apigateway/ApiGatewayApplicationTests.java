30e2562206f74c21bd17c75709e37d148179be30
Nguyen Huu Loc <locn562836@gmail.com>
2025-06-04 18:31:30 +0700

    chore: Create api-gateway module with initial configuration and application setup

--- api-gateway/src/test/java/vn/huuloc/apigateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/vn/huuloc/apigateway/ApiGatewayApplicationTests.java
@@ -1,13 +1,13 @@
-package vn.huuloc.gatewayservice;
+package vn.huuloc.apigateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GatewayServiceApplicationTests {
+class ApiGatewayApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
